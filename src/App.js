7efedf0c908a76c27ca892ea70bbdf753deae8b8
yuetongg <yuetong.23@ichat.sp.edu.sg>
2024-06-30 23:14:56 +0800

    Update routing for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import React, { useState } from 'react';
-import WalletConnector from './components/walletConnector';
-import QRCodeGenerator from './components/QRCodeGenerator';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import SendFunds from './components/SendFunds';
 
 const App = () => {
-  const [account, setAccount] = useState(null);
-  const contractAddress = "0xF984Eb190c4F6af1a3A8bA328884c8956A266f83"; // Replace with your actual contract address
-
   return (
-    <div>
-      <WalletConnector onConnected={setAccount} />
-      {account && <QRCodeGenerator contractAddress={contractAddress} />}
-    </div>
+    <Router basename="/klaytn_vestedlaunchpad">
+      <Switch>
+        <Route path="/send-funds/:recipientAddress?" component={SendFunds} />
+        {/* Other routes */}
+      </Switch>
+    </Router>
   );
 };
 
